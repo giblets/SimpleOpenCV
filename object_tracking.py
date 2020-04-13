2ff69fa4969f3b5b32c3e3144ae8b4ebf979b137
Glenn Martin <glenn.e.martin@gmail.com>
2020-04-13 13:54:33 -0400

    Initial checkin

---
+++ object_tracking.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+
+cap=cv2.VideoCapture(0)
+
+while (1):
+    ret,frame=cap.read()
+    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+
+    lowerLimit=np.array([38,100,100])
+    upperLimit=np.array([75,255,255])
+
+    mask=cv2.inRange(hsv,lowerLimit,upperLimit)
+
+    result=cv2.bitwise_and(frame,frame,mask=mask)
+
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('result',result)    
+
+    
+
+    if(cv2.waitKey(1) & 0xFF == ord('q')):
+        break
+
+cv2.destroyAllWindows()
